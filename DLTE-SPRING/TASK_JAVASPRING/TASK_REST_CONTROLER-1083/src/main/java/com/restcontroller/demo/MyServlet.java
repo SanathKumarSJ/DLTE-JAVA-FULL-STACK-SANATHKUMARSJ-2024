3b4b427a49cf41858aa713d95c7f97dac552e32a
SanathKumarSJ <sanathkumarsj@gmail.com>
2024-03-21 15:26:05 +0530

    REST_CONTROLLER

---
+++ DLTE-SPRING/TASK_JAVASPRING/TASK_REST_CONTROLER-1083/src/main/java/com/restcontroller/demo/MyServlet.java
@@ -0,0 +1,29 @@
+package com.restcontroller.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/loan")
+public class MyServlet {
+    List<Loan> loansList;
+    public MyServlet() {
+        loansList= Stream.of(new Loan(987455645L,25000L,"close","personal","Sanath",9745896321L),new Loan(7415855454545L,78000L,"open","personal","Rohith",9845564566666L),new Loan(78798454165L,96000L,"open","home","Mahesh",84654656664564L)).collect(Collectors.toList());
+    }
+
+    //get index from server and return the loan stored in that index
+    @GetMapping("/get/{index}")
+    public Loan readByIndex(@PathVariable("index") Integer index){
+        return loansList.get(index);
+    }
+
+    //adding new loan
+    @PostMapping("/add")
+    public String addNewLoan(@RequestBody Loan loan){
+        loansList.add(loan);
+        return loan+"added successfully";
+    }
+}
